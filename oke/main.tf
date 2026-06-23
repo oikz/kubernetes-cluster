b8a618212c1279d350feaa1b3bbd14ab9d837cdf
OikZ <chapmanjgm@gmail.com>
2026-06-23 17:26:37 +1200

    Add base terraform backend for OCI

---
+++ oke/main.tf
@@ -0,0 +1,13 @@
+﻿terraform {
+  required_providers {
+    oci = {
+      source  = "oracle/oci"
+      version = "~> 8.0"
+    }
+  }
+
+  backend "oci" {
+    bucket           = "oke-terraform"
+    namespace        = "axfqyu562wlb"
+  }
+}
