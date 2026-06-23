59a3c351fb068db1ba7817712d574baccb9eecb9
OikZ <chapmanjgm@gmail.com>
2026-06-24 10:54:55 +1200

    Mark variables as sensitive

--- oke/variables.tf
+++ oke/variables.tf
@@ -1,23 +1,29 @@
 ﻿variable "tenancy_ocid" {
   type = string
+  sensitive = true
 }
 variable "user_ocid" {
   type = string
+  sensitive = true
 }
 variable "fingerprint" {
   type = string
+  sensitive = true
 }
 variable "private_key" {
   type = string
+  sensitive = true
 }
 variable "region" {
   type = string
 }
 variable "compartment_id" {
   type = string
+  sensitive = true
 }
 variable "public_key" {
   type = string
+  sensitive = true
 }
 variable "timezone" {
   type = string
