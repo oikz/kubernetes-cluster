b327a719d79fe2e7af985726c77bb48876509dd0
OikZ <chapmanjgm@gmail.com>
2026-06-23 18:19:13 +1200

    Add baseline OKE cluster using the provided module
    Set to only one worker node with 1 core/6gb memory to fit within the free tier
    Set control plane to be public as this is just for testing, may set up VPN access in future
    Disabled bastion
    Enabled worker ssh access for testing purposes

---
+++ oke/oke.tf
@@ -0,0 +1,51 @@
+﻿module "oke" {
+  source  = "oracle-terraform-modules/oke/oci"
+  version = "5.4.3"
+
+  providers = {
+    oci      = oci
+    oci.home = oci.home
+  }
+
+  # Misc
+  compartment_id = var.compartment_id
+  tenancy_id     = var.tenancy_ocid
+  user_id        = var.user_ocid
+  home_region    = var.region
+  region         = var.region
+  timezone       = var.timezone
+
+  # Auth
+  api_fingerprint = var.fingerprint
+  api_private_key = var.private_key
+  ssh_public_key  = var.public_key
+
+  # Networking
+  create_bastion = false
+
+  # Workers
+  worker_pool_size = 1
+  worker_is_public = true
+  worker_pools = {
+    np1 = {
+      shape              = "VM.Standard.A1.Flex",
+      ocpus              = 1,
+      memory             = 6,
+      size               = 1,
+      boot_volume_size   = 50,
+      kubernetes_version = "v1.36.0"
+    }
+  }
+
+  # Security
+  allow_worker_internet_access = true
+  allow_worker_ssh_access      = true
+  control_plane_allowed_cidrs  = ["0.0.0.0/0"]
+  control_plane_is_public      = true
+
+  # Gateways
+  vcn_create_service_gateway = "never"
+  vcn_create_nat_gateway     = "never"
+
+  output_detail = true
+}
