f91d91c9d065e1705e1fdfdd5ecd5cfdcba01f7b
OikZ <chapmanjgm@gmail.com>
2026-06-23 17:50:56 +1200

    Use variables to set up OCI providers

--- oke/providers.tf
+++ oke/providers.tf
@@ -1,6 +1,16 @@
 ﻿provider "oci" {
+  tenancy_ocid     = var.tenancy_ocid
+  user_ocid        = var.user_ocid
+  fingerprint      = var.fingerprint
+  private_key_path = var.private_key
+  region           = var.region
 }
 
 provider "oci" {
-  alias = "home"
+  alias            = "home"
+  tenancy_ocid     = var.tenancy_ocid
+  user_ocid        = var.user_ocid
+  fingerprint      = var.fingerprint
+  private_key_path = var.private_key
+  region           = var.home_region
 }
---
+++ oke/variables.tf
@@ -0,0 +1,24 @@
+﻿variable "tenancy_ocid" {
+  type = string
+}
+variable "user_ocid" {
+  type = string
+}
+variable "compartment_id" {
+  type = string
+}
+variable "fingerprint" {
+  type = string
+}
+variable "private_key" {
+  type = string
+}
+variable "region" {
+  type = string
+}
+variable "home_region" {
+  type = string
+}
+variable "timezone" {
+  type = string
+}
