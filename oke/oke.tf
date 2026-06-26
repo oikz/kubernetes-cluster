d07dd567178264a3ebdb523ae521b53d27c51ba8
OikZ <chapmanjgm@gmail.com>
2026-06-26 17:49:32 +1200

    Update both to v1.35.2?

--- oke/oke.tf
+++ oke/oke.tf
@@ -14,7 +14,7 @@
   home_region        = var.region
   region             = var.region
   timezone           = var.timezone
-  kubernetes_version = "v1.35"
+  kubernetes_version = "v1.35.2"
 
   # Auth
   api_fingerprint = var.fingerprint
